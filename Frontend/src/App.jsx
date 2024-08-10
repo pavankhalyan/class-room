a8ef398aa053c1c7d42101763706ebe53f4577c7
pavan <pavankhalyan2004@gmail.com>
2024-08-10 12:23:52 +0530

    updating app.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,43 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import PrincipalDashboard from './Components/PrincipalDashboard'
+import TeacherDashboard from './Components/TeacherDashboard'
+import StudentDashboard from './Components/StudentDashboard' 
+import CreateClassroom from './Components/CreateClassroom'
+import CreateTimeTable from './Components/CreateTimeTable'
+import ClassroomTimeTable  from './Components/ClassroomTimeTable' 
+import StudentDetail from './Components/StudentDetail' 
+import StudentList from './Components/StudentList' 
+import TeacherDetail from './Components/TeacherDetail' 
+import TeacherList from './Components/TeacherList' 
+import Header from './Components/Header' 
+import Form from './Components/Form' 
+import Table from './Components/Table' 
+import Sidebar from './Components/Sidebar'
 
 function App() {
  
   return (
-    <div>
-      
-    </div>
+    <Router> 
+      <div className="App"> 
+        <Header /> 
+        <Sidebar /> 
+      <Routes>
+      <Route path='/principal' element={<PrincipalDashboard />} /> 
+      <Route path='/teacher' element={<TeacherDashboard />} />
+      <Route path='/student' element={<StudentDashboard />} />
+      <Route path='/create-classroom' element={<CreateClassroom />} /> 
+      <Route path='/create-timetable' element={<CreateTimeTable />} />
+      <Route path='/classroom-timetable' element={<ClassroomTimeTable />} /> 
+      <Route path='/student-detail' element={<StudentDetail />} /> 
+      <Route path='/student-list' element={<StudentList />} /> 
+      <Route path='/teacher-detail' element={<TeacherDetail />} /> 
+      <Route path='/teacher-list' element={<TeacherList />} />
+      <Route path='/form' element={<Form />} />
+      <Route path='/table' element={<Table />} />
+      </Routes>
+      </div>
+    </Router>
   )
 }
 
